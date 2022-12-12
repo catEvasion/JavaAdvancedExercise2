57d400801e47d9052da9c28a4435f77001622a5f
catherine.schlienger@hotmail.com <catherine.schlienger@hotmail.com>
2022-12-12 20:50:30 +0100

    innitial setup

---
+++ src/test/java/com/schlienger/MainTest.java
@@ -0,0 +1,13 @@
+package com.schlienger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test1(){
+        assertTrue(true);
+    }
+}
